a3628f3079343261d2a21af1ba999e07870628e6
Renán Zelaya <renan_zelaya@hotmail.com>
2021-03-18 11:22:57 -0600

    Export config options provider

--- src/modules/core-config/core-config.module.ts
+++ src/modules/core-config/core-config.module.ts
@@ -1,4 +1,4 @@
-import { DynamicModule, Module } from '@nestjs/common';
+import { DynamicModule, Module, Provider } from '@nestjs/common';
 import { CoreConfigService } from './core-config.service';
 import { CORE_CONFIG_OPTIONS } from './constants';
 import { CoreConfigModuleOptions } from './interfaces';
@@ -6,16 +6,15 @@ import { CoreConfigModuleOptions } from './interfaces';
 @Module({})
 export class CoreConfigModule {
   static register(options: CoreConfigModuleOptions): DynamicModule {
+    const optionProvider: Provider = {
+      provide: CORE_CONFIG_OPTIONS,
+      useValue: options,
+    };
+
     return {
       module: CoreConfigModule,
-      providers: [
-        {
-          provide: CORE_CONFIG_OPTIONS,
-          useValue: options,
-        },
-        CoreConfigService,
-      ],
-      exports: [CoreConfigService],
+      providers: [optionProvider, CoreConfigService],
+      exports: [optionProvider, CoreConfigService],
     };
   }
 }
